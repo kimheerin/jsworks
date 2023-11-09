8d71e9d0b35c70f6c045a8c9a218f07538184359
hheerriinn <hheerriinn@gmail.com>
2023-11-09 12:03:19 +0900

    231109

---
+++ day 2/js/if-else.js
@@ -0,0 +1,14 @@
+// if- else.js
+
+let num = 11;
+
+// 조건 연산자 (? :) - 짝수/홀수 판정
+let result = (num %2 == 0) ? "짝수" : "홀수";
+console.log(result);
+
+//if else 문
+if(num % 3 == 0){
+  document.write("3의 배수입니다.")
+}else{
+  document.write("3의 배수가 아닙니다.")
+}
